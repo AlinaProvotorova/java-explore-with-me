90aac31c4fcf47d4d6699bbd3d0df6746c3128d7
AlinaProvotorova <provotorovaalin@yandex.ru>
2023-09-25 13:09:45 +0300

    add errorHendler in stat-server

---
+++ stats-service/stat-server/src/main/java/ru/practicum/stats/ErrorMessage.java
@@ -0,0 +1,10 @@
+package ru.practicum.stats;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorMessage {
+    private final String error;
+}
---
+++ stats-service/stat-server/src/main/java/ru/practicum/stats/utils/Marker.java
@@ -0,0 +1,14 @@
+package ru.practicum.stats.utils;
+
+public interface Marker {
+
+    interface OnCreate {
+    }
+
+    interface OnUpdate {
+    }
+
+    interface OnUpdateAdmin {
+    }
+
+}
